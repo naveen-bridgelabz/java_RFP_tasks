bd502a75e22b1677402af68026ae81aef3ae1ddc
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-05-16 12:32:50 +0530

    added UC1

--- Day_03/LineComparisonProblem/LineComparison.java
+++ Day_03/LineComparisonProblem/LineComparison.java
@@ -1,5 +1,19 @@
 public class LineComparison {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        int x1 = 2;
+        int y1 = 4;
+        int x2 = 6;
+        int y2 = 8;
+
+        double length = Math.sqrt(
+                Math.pow((x2 - x1), 2) +
+                        Math.pow((y2 - y1), 2)
+        );
+
+        System.out.println("Length of Line: " + length);
     }
 }
