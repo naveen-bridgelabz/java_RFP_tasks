b0667a6710ff353535a00ade2ff870e86cab38c9
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-05-16 13:17:17 +0530

    Modified main structure

---
+++ Day_03/src/LineComparisonProblem/LineComparison.java
@@ -0,0 +1,10 @@
+package LineComparisonProblem;
+
+public class LineComparison {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Program");
+
+    }
+}
