c08e86b499f2f26aff896ccf2912ef0bd4992cfa
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-05-19 11:46:34 +0530

    add: implement gambling simulator uc1

---
+++ Day_07/src/GamblingSimulation/GamblingSimulator.java
@@ -0,0 +1,17 @@
+package GamblingSimulation;
+
+public class GamblingSimulator {
+
+    static final int STAKE = 100;
+
+    static final int BET = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Gambling Simulator");
+
+        System.out.println("Initial Stake: $" + STAKE);
+
+        System.out.println("Bet Amount Per Game: $" + BET);
+    }
+}
