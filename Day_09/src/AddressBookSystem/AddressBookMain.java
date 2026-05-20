c905e1e8c49f65ae6cc85d5c61aeab765e41b9c5
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-05-20 12:39:53 +0530

    add: implement address book uc1

--- Day_09/src/AddressBookSystem/AddressBookMain.java
+++ Day_09/src/AddressBookSystem/AddressBookMain.java
@@ -5,5 +5,25 @@ public class AddressBookMain {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
+
+        Contact person = new Contact();
+
+        person.firstName = "Naveen";
+
+        person.lastName = "Kumar";
+
+        person.address = "Chennai";
+
+        person.city = "Chennai";
+
+        person.state = "Tamil Nadu";
+
+        person.zip = "600001";
+
+        person.phoneNumber = "9876543210";
+
+        person.email = "naveen@gmail.com";
+
+        person.displayContact();
     }
 }
